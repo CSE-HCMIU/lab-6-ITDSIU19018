9af1d546cc97952f5d5e0c3ab3f62cd0173bfd24
ITDSIU19018 <69339342+ITDSIU19018@users.noreply.github.com>
2020-08-07 15:43:45 +0700

    Update Exercise2.c

--- Exercise2.c
+++ Exercise2.c
@@ -11,7 +11,55 @@ Ex:
 #include <stdlib.h>
 
 int ex2(int n, char *str[]){
-	//your codes here
+	int ind[n],i,j;
+	for(i = 0;i<n;i++)
+	{
+			if(strcmp(str[i], "one") == 0)
+				ind[i] = 1;
+			else if(strcmp(str[i], "two") == 0)
+				ind[i] = 2;				
+			else if(strcmp(str[i], "three") == 0)
+				ind[i] = 3;
+			else if(strcmp(str[i], "four") == 0)
+				ind[i] = 4;
+			else if(strcmp(str[i], "five") == 0)
+				ind[i] = 5;
+			else if(strcmp(str[i], "six") == 0)
+				ind[i] = 6;
+			else if(strcmp(str[i], "seven") == 0)
+				ind[i] = 7;
+			else if(strcmp(str[i], "eight") == 0)
+				ind[i] = 8;
+			else if(strcmp(str[i], "nine") == 0)
+				ind[i] = 9;
+			else if(strcmp(str[i], "ten") == 0)
+				ind[i] = 10;
+			else if(strcmp(str[i], "zero") == 0)
+				ind[i] = 0;
+		}
+	int tmp;
+	char *temp;
+    for(i = 0; i < n - 1; i++)
+	{
+        for(j = i + 1; j < n; j++)
+		{
+            if(ind[i] > ind[j]){
+               
+                tmp = ind[i];
+                ind[i] = ind[j];
+                ind[j] = tmp;   
+               
+				temp = str[i];
+                str[i] = str[j];
+                str[j] = temp; 				
+            }
+        }
+    }
+	for(i = 0; i < n ; i++)
+	{
+		printf("%s ", str[i]);
+	}
+
 }
 
 int main(int argc, char *argv[]) {
